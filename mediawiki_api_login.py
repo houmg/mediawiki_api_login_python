68496bdb6f8e3fd211c0e603f693622f01717bda
houmg <hou_mg@126.com>
2018-04-08 18:23:54 +0800

    Update mediawiki_api_login.py
    Crawl from mediawiki

--- mediawiki_api_login.py
+++ mediawiki_api_login.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
 import requests
+from bs4 import BeautifulSoup
+from urllib.request import urlopen 
+import re
 
 username = 'username'
 password = 'password'
+url = 'http://exemple.com'
 base_url = 'http://exemple.com/mediawiki/' 
 api_url = base_url+'api.php'
 
@@ -31,4 +35,23 @@ if r2.json()['login']['result'] != 'Success':
 r3 = session.get(base_url,cookies=r2.cookies)
 
 # Display the HTML
-print(r3.text)
+
+pages = set()
+def getlinks(articleUrl):
+    bsobj = BeautifulSoup(articleUrl,"html.parser")
+    links = bsobj.findAll("a",href=re.compile("^/mediawiki/((?!Special|png|index\.php|User|Talk|Sidebar|MagicBook|%E9%A6%96%E9%A1%B5|jpg).)*$"))
+    for link in links:
+        if 'href' in link.attrs:
+            if link.attrs['href'] not in pages:
+                newpage = link.attrs['href']
+                pages.add(newpage)
+                path = (link.get_text()+".html")
+                r = requests.get(url+newpage,cookies=r2.cookies)
+                with open(path,"wb") as f:
+                    f.write(r.content)
+                f.close()
+                print(path)
+                getlinks((session.get(url+newpage,cookies=r2.cookies).text))
+
+articles = getlinks(r3.text)
+
